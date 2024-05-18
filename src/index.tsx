69cdba9fd4867d88810f90e4a4c39c87616fc802
Ashutosh <ashu.singh212@gmail.com>
2024-05-18 17:26:47 +0530

    fix: base name

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`/${process.env.BASE_NAME}`}>
       <AppProvider>
         <App />
       </AppProvider>
