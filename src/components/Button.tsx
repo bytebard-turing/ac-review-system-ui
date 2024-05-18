d2b74a31695e57f7cb5d74bb18c767fc922de345
Ashutosh <ashu.singh212@gmail.com>
2024-05-18 17:37:28 +0530

    Updates

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ButtonHTMLAttributes } from 'react';
-
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
-  return <button {...rest}>{children}</button>;
-};
-
-export default Button;
--- src/hooks/useEffectIf.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-export const useEffectIf = (condition: Boolean, action: () => void): void => {
-  useEffect(() => {
-    if (condition) {
-      action()
-    }
-  }, [action, condition])
-}
